6ca5078d5a12c854e0d2065e5316f90df2dd2346
Temitope Akinsoto <takinsoto@gmail.com>
2020-06-07 01:30:01 +0100

    Re-design marketing page
    In this commit, a background image was place on the banner section
    of the marketing page. Also, the navigation bar was tweaked

--- tipsease/src/components/Marketing.jsx
+++ tipsease/src/components/Marketing.jsx
@@ -10,139 +10,137 @@ import homescreen from "../images/home-screen.png";
 
 export default function Marketing() {
   return (
-    <div>
-      <body>
-        <section>
-          <nav>
-            <h3 className="logo">
-              <a href="#">Tipsease</a>
-            </h3>
-            <ul className="nav-list">
-              <li className="list-item">
-                <a href="/register">Register</a>
-              </li>
-              <li className="list-item">
-                <a href="/login">Login</a>
-              </li>
-            </ul>
-          </nav>
-        </section>
-        <section className="banner">
-          <div className="banner-header">
-            <h1>The Tipping App For A Cashless World</h1>
-            <button>Get Started</button>
+    <body>
+      <section className="navigation">
+        <nav>
+          <h3 className="logo">
+            <a href="#">Tipsease</a>
+          </h3>
+          <ul className="nav-list">
+            <li className="list-item">
+              <a href="/register">Register</a>
+            </li>
+            <li className="list-item">
+              <a href="/login">Login</a>
+            </li>
+          </ul>
+        </nav>
+      </section>
+      <section className="banner">
+      <div className="banner-text">
+          <h1>The Tipping App For A Cashless World</h1>
+          <button>Get Started</button>
+        </div>
+        <div className="banner-overlay">
+        
+        </div>
+      </section>
+      <section className="service-workers">
+        <div className="service-workers-heading">
+          <h1>For Service Workers</h1>
+          <p>
+            Earn More Money With Tipsease And Let Your Tips Jar Runneth Over
+          </p>
+        </div>
+        <div className="service-worker-content">
+          <div>
+            <img src={tipsimg} />
           </div>
-          <div className="banner-item">
-            <img src={homescreen} alt="" />
-          </div>
-        </section>
-        <section className="service-workers">
-          <div className="service-workers-heading">
-            <h1>For Service Workers</h1>
+          <div className="service-worker-features">
             <p>
-              Earn More Money With Tipsease And Let Your Tips Jar Runneth Over
+              <img className="feature-icons" src={wallet} /> Never miss an
+              opportunity to get tipped
+            </p>
+            <p>
+              <img className="feature-icons" src={car} alt="" /> Get paid by
+              anyone, anywhere, anytime
+            </p>
+
+            <p>
+              <img className="feature-icons" src={cash} alt="" /> Get paid
+              faster and more often
+            </p>
+            <p>
+              <img className="feature-icons" src={smartmoney} alt="" /> Earn
+              Bonuses through our Referral Incentives
             </p>
           </div>
-          <div className="service-worker-content">
+        </div>
+      </section>
+
+      <section className="users">
+        <div className="service-workers-heading">
+          <h1>For Users</h1>
+          <p>
+            No Cash...? No Problem! With Tipsease You Just Need Your Phone To
+            Tip Anyone Anywhere
+          </p>
+        </div>
+        <div className="service-worker-content">
+          <div className="service-worker-features">
             <div>
-              <img src={tipsimg} />
-            </div>
-            <div className="service-worker-features">
               <p>
-                <img className="feature-icons" src={wallet} /> Never miss an
-                opportunity to get tipped
+                <img className="feature-icons" src={wallet} alt="" /> Fast as
+                paying with cash from a wallet or money clip
               </p>
               <p>
-                <img className="feature-icons" src={car} alt="" /> Get paid by
-                anyone, anywhere, anytime
+                <img className="feature-icons" src={car} alt="" /> It's easy to
+                setup, transact & administer for the Tipper & Tippee
               </p>
 
               <p>
-                <img className="feature-icons" src={cash} alt="" /> Get paid
-                faster and more often
-              </p>
-              <p>
-                <img className="feature-icons" src={smartmoney} alt="" /> Earn
-                Bonuses through our Referral Incentives
+                <img className="feature-icons" src={cash} alt="" /> It's secure
+                as a paypal or credit card transaction
               </p>
             </div>
-          </div>
-        </section>
-
-        <section className="users">
-          <div className="service-workers-heading">
-            <h1>For Users</h1>
             <p>
-              No Cash...? No Problem! With Tipsease You Just Need Your Phone To
-              Tip Anyone Anywhere
+              <img className="feature-icons" src={smartmoney} alt="" /> It's
+              anonymous and there's no need to know each other
             </p>
           </div>
-          <div className="service-worker-content">
-            <div className="service-worker-features">
-              <div>
-                <p>
-                  <img className="feature-icons" src={wallet} alt="" /> Fast as
-                  paying with cash from a wallet or money clip
-                </p>
-                <p>
-                  <img className="feature-icons" src={car} alt="" /> It's easy
-                  to setup, transact & administer for the Tipper & Tippee
-                </p>
-
-                <p>
-                  <img className="feature-icons" src={cash} alt="" /> It's
-                  secure as a paypal or credit card transaction
-                </p>
-              </div>
-              <p>
-                <img className="feature-icons" src={smartmoney} alt="" /> It's
-                anonymous and there's no need to know each other
-              </p>
-            </div>
-            <div>
-              <img src={tipsimg} alt="" />
-            </div>
+          <div>
+            <img src={tipsimg} alt="" />
           </div>
-        </section>
+        </div>
+      </section>
 
-        <section className="service-workers">
-          <div className="service-workers-heading">
-            <h1>For Businesses</h1>
+      <section className="service-workers">
+        <div className="service-workers-heading">
+          <h1>For Businesses</h1>
+          <p>
+            Motivate your service team with more tips, improved team-work and
+            better customer relations
+          </p>
+        </div>
+        <div className="service-worker-content">
+          <div>
+            <img src={tipsimg} alt="" />
+          </div>
+          <div className="service-worker-features">
             <p>
-              Motivate your service team with more tips, improved team-work and
-              better customer relations
+              <img className="feature-icons" src={wallet} alt="" /> Never miss
+              an opportunity to get tipped
+            </p>
+            <p>
+              <img className="feature-icons" src={car} alt="" /> Get paid by
+              anyone, anywhere, anytime
+            </p>
+
+            <p>
+              <img className="feature-icons" src={cash} /> Get paid faster and
+              more often
+            </p>
+            <p>
+              <img className="feature-icons" src={smartmoney} /> Earn Bonuses
+              through our Referral Incentives
             </p>
           </div>
-          <div className="service-worker-content">
-            <div>
-              <img src={tipsimg} alt="" />
-            </div>
-            <div className="service-worker-features">
-              <p>
-                <img className="feature-icons" src={wallet} alt="" /> Never miss
-                an opportunity to get tipped
-              </p>
-              <p>
-                <img className="feature-icons" src={car} alt="" /> Get paid by
-                anyone, anywhere, anytime
-              </p>
+        </div>
+      </section>
 
-              <p>
-                <img className="feature-icons" src={cash} /> Get paid faster and
-                more often
-              </p>
-              <p>
-                <img className="feature-icons" src={smartmoney} /> Earn Bonuses
-                through our Referral Incentives
-              </p>
-            </div>
-          </div>
-        </section>
-
-        <footer className="footer">
-          <p>Copyright {new Date().getFullYear()}</p>
-        </footer>
-      </body>
-    </div>
+      <footer className="footer">
+        <p>Copyright {new Date().getFullYear()}</p>
+      </footer>
+    </body>
   );
 }
